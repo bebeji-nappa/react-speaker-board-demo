0103c272c7a41b4c1e8dcabafd2d8ddb7704cba7
bebeji-nappa <masadecade@gmail.com>
2021-10-08 19:34:05 +0900

    create website

--- src/slideboard/MainTitle.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./MainTitle.css"
-
-export const MainTitle:React.VFC<MainTitle> = React.memo(({children, textAlign = "left"}) => {
-  return (
-    <h1 className={`MainTitle${textAlign && ` ${textAlign}`}`}>{children}</h1>
-  )
-})
--- src/slideboard/spacer.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import "./spacer.css"
-
-export const Spacer: React.VFC = () => {
-  return (
-    <div className="spacer"></div>
-  )
-}
