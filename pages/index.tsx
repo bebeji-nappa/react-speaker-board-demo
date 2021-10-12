c33aa90a442b5e78f5b034bc265327457051b1b7
bebeji-nappa <masadecade@gmail.com>
2021-10-12 22:40:41 +0900

    Add Meta Data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
 import { NextPage } from 'next'
 import { ReactSpeakerBoard } from "react-speaker-board"
 import { Slide } from "../slide/Slide"
-
+import MetaData from '../components/head'
 const Home: NextPage = () => {
-  return <ReactSpeakerBoard slide={Slide} mode="slide" />
+  return (
+    <>
+      <MetaData />
+      <ReactSpeakerBoard slide={Slide} mode="slide" />
+    </>
+  )
 }
 
 export default Home
