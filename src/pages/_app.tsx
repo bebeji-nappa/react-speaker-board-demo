c8e7175eeb9c231846ffdf13a220b752eee80bb6
bebeji-nappa <masadecade@gmail.com>
2024-02-10 02:50:24 +0900

    fix import css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../../styles/globals.css';
 import '../../styles/docs.css';
 import '../../styles/slide.css';
+import "react-speaker-board/styles/ReactSpeakerBoard.css";
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
