2923eb3060c7ad48ad0a5b78e84fb5b715776d5b
bebeji-nappa <masadecade@gmail.com>
2024-02-10 01:06:42 +0900

    fix path

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import '../../styles/globals.css';
-import '../../styles/Slide.css';
+import '../../styles/slideStyle.css';
 import '../../styles/docs.css';
 
 export default function MyApp({ Component, pageProps }) {
