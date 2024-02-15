586b9804a417c6f948a2798a149acb072810eabf
bebeji-nappa <masadecade@gmail.com>
2024-02-16 05:12:22 +0900

    richmd アップデート

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import '../../styles/globals.css';
 import '../../styles/docs.css';
 import '../../styles/slide.css';
 import "react-speaker-board/styles/reactSpeakerBoard.css";
+import "richmd/richmd.css";
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
